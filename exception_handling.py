3766484a3ced18346f5b6e78eec526ade748cc32
Malibongwe <159195081+Mali-bee@users.noreply.github.com>
2025-07-15 16:21:05 +0200

    Update exception_handling.py

--- exception_handling.py
+++ exception_handling.py
@@ -1,4 +1,7 @@
 #learning exception handling for python
+#TRY - to test a block of code for errors
+#ELSE - excecute code when ther is no error
+#FINALLy - to execute code regardless of the result of the TRY and EXCEPT blocks
 
 try:
   print(x)
